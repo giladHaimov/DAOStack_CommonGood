3e318aa1c49640df634caceb08a950af19e0fc16
gilad <giladha@gmail.com>
2022-10-16 07:09:43 +0300

    move secret file to relative path

--- truffle-config.js
+++ truffle-config.js
@@ -21,9 +21,8 @@
  */
 
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-//var HDWalletProvider = require("truffle-hdwallet-provider");
 const fs = require('fs');
-const mnemonic = fs.readFileSync("/Users/GiladMac/secret_code.txt").toString().trim();
+const mnemonic = fs.readFileSync("../secret.txt").toString().trim();
 
 module.exports = {
 
